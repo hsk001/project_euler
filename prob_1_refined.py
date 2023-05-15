0725ad1501f317b0ae8a92855eba424ab542c65d
hsk001 <65063557+hsk001@users.noreply.github.com>
2023-05-15 10:58:14 +0530

    Update prob_1_refined.py

--- prob_1_refined.py
+++ prob_1_refined.py
@@ -1,1 +1,8 @@
+def sum_of_multiples(n, k):
+    p = (n - 1) // k
+    return k * p * (p + 1) // 2
+
+sum = sum_of_multiples(1000, 3) + sum_of_multiples(1000, 5) - sum_of_multiples(1000, 15)
+
+print("The sum of all the multiples of 3 or 5 below 1000 is:", sum)
 
